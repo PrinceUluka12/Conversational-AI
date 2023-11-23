3b3543b33acc5aa7372b51699e594abed1ff4f8a
Prince <prince.uluka@icloud.com>
2023-11-22 19:53:44 -0800

    Fixes

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/fulfillment', methods=['POST'])
-def fulfillment():
+# Route to return student number in JSON format
+@app.route('/')
+def get_student_number():
+    student_number = "200501406" 
+    response_data = {
+        'student_number': student_number
+    }
+    return jsonify(response_data)
+
+# Route for Dialogflow webhook
+@app.route('/webhook', methods=['POST'])
+def dialogflow_webhook():
     data = request.get_json()
 
     # Extract parameters from the request
@@ -12,11 +22,13 @@ def fulfillment():
 
     # Perform fulfillment logic
     # For simplicity, let's just echo back the parameters
-    response = {
-        'fulfillmentText': f'You said: {intent}. Parameters: {parameters}',
+    response_text = f'You said: {intent}. Parameters: {parameters}'
+
+    response_data = {
+        'fulfillmentText': response_text,
     }
 
-    return jsonify(response)
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
