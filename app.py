a171cbb0c815fce75b5a1e9d6a77ecb64b624fba
Prince <prince.uluka@icloud.com>
2023-11-22 19:36:04 -0800

    fixes

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, jsonify
-import os
-
 
 app = Flask(__name__)
 
@@ -21,5 +19,4 @@ def fulfillment():
     return jsonify(response)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
